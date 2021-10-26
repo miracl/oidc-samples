78164bb9cb09c0e8c23858740e355a471248d858
Vladislav Mitov <vladislav.mitov@gmail.com>
2021-10-27 00:20:09 +0300

    Refactor Proxy Dockerfile

--- proxy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-COMMAND="proxy"
-
-if [ -n "$VERBOSE" ]; then COMMAND="${COMMAND} -v"; fi
-if [ -n "$HOST" ]; then COMMAND="${COMMAND} --host=${HOST}"; fi
-if [ -n "$PORT" ]; then COMMAND="${COMMAND} --port=${PORT}"; fi
-
-eval $COMMAND
