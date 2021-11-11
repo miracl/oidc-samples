ec699b1329ec34a98e4bdc92c5261d0137a27052
Petya Koleva <petya.koleva@miracl.com>
2021-11-11 10:04:30 +0200

    Build integration tests for Windows

---
+++ integration-tests/entrypoint-win.ps1
@@ -0,0 +1,11 @@
+$file = $Env:windir+'\System32\drivers\etc\hosts'; $Env:SAMPLE_IP+' sample' | Add-Content -PassThru $file;
+
+# this will validate that we have a proper set in the hosts file and access the sample ip
+$connection = New-Object System.Net.Sockets.TcpClient("sample", 8000)
+if ($connection.Connected) {
+    go test -v -mod=vendor . --client-id $Env:CLIENT_ID --client-secret $Env:CLIENT_SECRET --sample-url $Env:SAMPLE_URL --redirect-url $Env:REDIRECT_URL
+    exit $LASTEXITCODE
+} else {
+    Write-Host "Tests cannot be run as the sample's ip $Env:SAMPLE_IP is not accessible"
+    exit 1
+}
